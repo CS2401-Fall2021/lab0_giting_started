de85bf0261d9716fb801813c3ac71b9581094d93
m-steph <86425576+m-steph@users.noreply.github.com>
2021-08-13 16:02:59 -0600

    Update DebuggingExamples.java

--- DebuggingExamples.java
+++ DebuggingExamples.java
@@ -1,1 +1,32 @@
 
+public class Debugging Examples {
+
+    public static void main(String[] args)
+        System.out.println(mpg(470,16));
+        multiplier()
+
+    }
+    public static doubel mpg(int miles, int gallons){
+        double mpg = gallons / miles;
+        return mpg;
+    }
+
+    public static multiplier(){
+        / / declare data
+        double firstnum;
+        double secondnum;
+        double product;
+        // input data
+        Scanner in = new Scanner( System.in );
+        System.out.print( "Enter first number to multiply: " );
+        double firstnum = in.nextdouble( );
+        System.out.print( "Enter second number to multiply: " );
+        double secondnum = in.nextdouble( );
+        // calculate result
+        product = 2ndnumber * 1stnumber;
+        // output results
+        System.out.print( firstNum + " * " );
+        System.out.print( secondNum + " = " );
+        System.out.println( product );
+    }
+}
